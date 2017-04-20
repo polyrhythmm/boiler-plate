4d7bf0674e532515c42808f3393d7f6ecd421672
Andrew Day <Andrew Day>
2017-04-21 00:54:26 +1000

    updated server.js

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function(request, response, next){
+  if(request.headers['x-forwarded-proto'] === 'https'){
+    response.redirect('http://' + request.hostname + request.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-    console.log("Express server is up on port 3000");
+
+app.listen(PORT, function() {
+    console.log("Express server is up on port " + PORT);
 })
